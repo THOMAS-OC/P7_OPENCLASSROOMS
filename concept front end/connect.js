16c428aecb012a3f048e6bcb7a31736610b5fe8c
= <=>
2022-06-21 17:26:36 +0000

    Graphisme géométrique page d'accueil + démarrage de la page post

--- concept front end/connect.js
+++ concept front end/connect.js
@@ -1,4 +1,5 @@
 const button = document.querySelector("input[type='submit']")
 button.addEventListener("click", el => {
     el.preventDefault()
+    window.location.assign("http://127.0.0.1:5500/home.html")
 })
