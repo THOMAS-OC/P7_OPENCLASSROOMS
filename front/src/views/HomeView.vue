82ddce32c2e8b424ab4535a3c87604e428547544
= <=>
2022-07-25 21:01:26 +0200

    Mise en page d'un bouton pour ajouter un post

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -2,6 +2,11 @@
   <div class="home">
     <h1>Bienvenu sur le forum de Groupomania</h1>
 
+    <div class="createPost">
+      <div></div>
+      <div></div>
+    </div>
+
     <article v-for="post in posts" :key="post.id">
 
       <the-post :authorPost="post.name" :contentPost="post.content" :datePost="post.date" :postId="post.id"></the-post>
@@ -71,6 +76,43 @@ export default {
   button{
     top: 0;
     left: 0;
+  }
+
+  .createPost{
+    cursor: pointer;
+    margin: 50px auto;
+    width: 250px;
+    height: 250px;
+    border-radius: 50%;
+    border: 3px solid #FD2D01;
+    background-color: white;
+    position: relative;
+    transition-duration: 0.5s;
+  }
+
+  .createPost:hover{
+    transform: scale(1.1);
+    box-shadow: 0px 0px 30px #FD2D01;
+  }
+
+  .createPost:hover div{
+    background-color:#4E5166;
+  }
+
+  .createPost div{
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, 50%) scale(0.8);
+    width: 100px;
+    height: 8px;
+    background-color:#7e7f8a;
+    transition-duration: 1s;
+    border-radius: 5px;
+  }
+
+  .createPost div:nth-child(2){
+    transform: translate(-50%, 50%) rotate(90deg) scale(0.8);
   }
 
 </style>
