982f49752186911ce5d977f6f2ecd985a3cc0acb
= <=>
2022-08-12 20:25:48 +0200

    Gestion de l'abscence de données : a generaliser sur les validateurs

--- back/middleware/registerValidator.js
+++ back/middleware/registerValidator.js
@@ -4,20 +4,21 @@ module.exports = (req, res, next) => {
 
     console.log("validateur register");
 
-    let name = req.body.name.trim()
+    let name = req.body.name || false
     let nameRegex = /^[A-Za-zéàèêëï]{3,30}$/
 
-    let firstName = req.body.firstname.trim()
+    let firstName = req.body.firstname || false
     let FirstNameRegex = /^[A-Za-zéàèêëï]{3,20}$/
     
-    let email = req.body.email.trim()
+    let email = req.body.email || false
     let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
 
-    if (!email.toLowerCase().match(emailRegex)) res.status(400).json({message : "Email invalide"})
 
-    else if (!name.toLowerCase().match(nameRegex)) res.status(400).json({message : "Nom invalide"})
+    if (!email || !email.trim().toLowerCase().match(emailRegex)) res.status(400).json({message : "Email invalide ou inexistant"})
+
+    else if (!name || !name.trim().toLowerCase().match(nameRegex)) res.status(400).json({message : "Nom invalide ou inexistant"})
     
-    else if (!firstName.toLowerCase().match(FirstNameRegex)) res.status(400).json({message : "Prénom invalide"})
+    else if (!firstName || !firstName.trim().toLowerCase().match(FirstNameRegex)) res.status(400).json({message : "Prénom invalide ou inexistant"})
 
     else next()
             
